816810bd8fe11989b70cda6b9b965d092d437626
Tyler Coles <tyler@tyev.co>
2023-09-17 21:07:02 -0400

    Attempting to start the dice detector; Added simple camera follow

--- Scripts/CameraMovement.cs
+++ Scripts/CameraMovement.cs
@@ -4,5 +4,19 @@ namespace PleaseKissMyElbow;
 
 public partial class CameraMovement : Camera3D
 {
-	
+    [Export] public Node TrackingNode { get; set; }
+
+    [Export] public Vector3 Offset { get; set; }
+
+    public override void _Process(double delta)
+    {
+        var cameraMount = TrackingNode.GetNode("cameraMount");
+        if (this.GetParent() != cameraMount)
+        {
+            this.GetParent().RemoveChild(this);
+            cameraMount.AddChild(this);
+        }
+
+        // GD.Print(Position);
+    }
 }
