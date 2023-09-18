248037924f12bbb4d658875f2dc1fc4bbe0b04c4
Tyler Coles <tyler@tyev.co>
2023-09-18 13:58:48 -0400

    Adding mouse rotation to camera

--- Scripts/CameraMovement.cs
+++ Scripts/CameraMovement.cs
@@ -2,11 +2,34 @@ using Godot;
 
 namespace PleaseKissMyElbow;
 
-public partial class CameraMovement : Camera3D
+public partial class CameraMovement : Node3D
 {
 	[Export] public Node TrackingNode { get; set; }
 
 	[Export] public Vector3 Offset { get; set; }
+	[Export] public float HorizontalMouseSensitivity { get; set; } = 0.25f;
+	[Export] public float VerticalMouseSensitivity { get; set; } = 0.25f;
+	
+	private Node3D Camera { get; set; }
+	
+	public override void _Ready()
+	{
+		Input.MouseMode = Input.MouseModeEnum.Captured;
+		Camera = GetNode("Camera3D") as Node3D;
+	}
+
+	public override void _Input(InputEvent @event)
+	{
+		if (@event is InputEventMouseMotion mouseMotion)
+		{
+			RotateY(Mathf.DegToRad(mouseMotion.Relative.X * HorizontalMouseSensitivity));
+			Camera.RotateX(Mathf.DegToRad(mouseMotion.Relative.Y * VerticalMouseSensitivity));
+			
+			GD.Print($"{mouseMotion.Relative}");
+		}
+		
+		GD.Print($"{Camera.Position}; {Camera.Rotation} | {Position}; {Rotation}");
+	}
 
 	public override void _Process(double delta)
 	{
@@ -19,10 +42,4 @@ public partial class CameraMovement : Camera3D
 
 		// GD.Print(Position);
 	}
-	
-	private void _on_d_20_dice_rolled(int result, Dice dice)
-	{
-		// Replace with function body.
-	}
-
 }
